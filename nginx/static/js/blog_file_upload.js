6ad68be02d4fa444cc5dfd250165385b69f9c7c9
nas user <pi@nas.localnet.jp>
2021-02-28 02:49:00 +0900

    コードの整形

--- nginx/static/js/blog_file_upload.js
+++ nginx/static/js/blog_file_upload.js
@@ -1,16 +1,17 @@
 document.addEventListener('DOMContentLoaded', e => {
     const upload = (uploadFile, textarea) => {
         const insertText = (textarea, text) => {
-            const cursorPos      = textarea.selectionStart;
+            const cursorPos = textarea.selectionStart;
             const last      = textarea.value.length;
-            const before   = textarea.value.substr(0, cursorPos);
-            const after    = textarea.value.substr(cursorPos, last);
-            textarea.value = before + text + after;
+            const before    = textarea.value.substr(0, cursorPos);
+            const after     = textarea.value.substr(cursorPos, last);
+            textarea.value  = before + text + after;
         };
-        const getCookie = name => {
+        const getCookie = (name) => {
             if (document.cookie && document.cookie !== '') {
                 for (const cookie of document.cookie.split(';')) {
                     const [key, value] = cookie.trim().split('=');
+
                     if (key === name) {
                         return decodeURIComponent(value);
                     }
@@ -33,12 +34,11 @@ document.addEventListener('DOMContentLoaded', e => {
 
             if (['png', 'jpg', 'gif', 'jpeg', 'bmp'].includes(extension)) {
                 html = `![](${response.url})`;
-            } else {
+            }
+            else {
                 html= `[](${response.url})`;
             }
-
             insertText(textarea, html);
-
         }).catch(error => {
             console.log(error);
         });
