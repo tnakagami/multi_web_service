fc87fd7d143c6f3ca1a9bc02c43f6949fbc3c716
nas user <pi@nas.localnet.jp>
2021-03-06 13:21:20 +0900

    アップロード用のアプリを追加。

---
+++ django/src/storage/forms.py
@@ -0,0 +1,55 @@
+from django import forms
+from django.template.defaultfilters import filesizeformat
+from django.utils.translation import ugettext_lazy
+from . import models
+
+class UploadFileForm(forms.ModelForm):
+    file = forms.FileField(
+        label=ugettext_lazy('upload file'),
+        widget=forms.FileInput(attrs={'class': 'custom-file-input'}),
+    )
+
+    class Meta:
+        model = models.FileStorage
+        fields = ('file', )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.MAX_UPLOAD_SIZE = 100 * 1024 * 1024
+
+    def clean(self):
+        cleaned_data = super().clean()
+        file = cleaned_data.get('file')
+
+        try:
+            # check file size
+            if file.size > self.MAX_UPLOAD_SIZE:
+                max_size = filesizeformat(str(self.MAX_UPLOAD_SIZE))
+                current_size = filesizeformat(file.size)
+                raise forms.ValidationError(ugettext_lazy('Please keep filesize under {}. Current filesize {}'.format(max_size, current_size)))
+        except AttributeError:
+            pass
+
+        return cleaned_data
+
+class FileSearchForm(forms.Form):
+    """
+    file searching form
+    """
+    search_word = forms.CharField(
+        label=ugettext_lazy('filename'),
+        required=True,
+        widget=forms.TextInput(
+            attrs={'placeholder': ugettext_lazy('filename'), 'class': 'form-control'}
+        ),
+    )
+
+    def filtered_queryset(self, queryset):
+        # get search word
+        search_word = self.cleaned_data.get('search_word')
+
+        if search_word:
+            for word in search_word.split():
+                queryset = queryset.filter(file__icontains=word)
+
+        return queryset
