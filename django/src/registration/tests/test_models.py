8facdab15f9f1c272b38b75f91663035d4340416
nas user <pi@nas.localnet.jp>
2021-03-09 00:30:27 +0900

    registrationのformのテストケースを追加。

--- django/src/registration/tests/test_models.py
+++ django/src/registration/tests/test_models.py
@@ -4,21 +4,23 @@ from registration.tests.factories import UserFactory, UserModel
 
 # sample
 class UserModelTests(TestCase):
-    def setUp(self):
-        self.normal_user = UserFactory(username='alice', viewname='_alice')
-        self.blank_viewname_user = UserFactory(viewname='')
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
 
     def test_create_valid_user(self):
-        self.assertEqual(self.normal_user.username, 'alice')
-        self.assertEqual(self.normal_user.get_full_name(), 'alice')
-        self.assertEqual(self.normal_user.get_short_name(), 'alice')
-        self.assertEqual(self.normal_user.viewname, '_alice')
-        self.assertEqual(self.normal_user.email, 'alice@example.com')
-        self.assertFalse(self.normal_user.is_staff)
-        self.assertTrue(self.normal_user.is_active)
+        normal_user = UserFactory(username='alice', viewname='_alice')
+        self.assertEqual(normal_user.username, 'alice')
+        self.assertEqual(normal_user.get_full_name(), 'alice')
+        self.assertEqual(normal_user.get_short_name(), 'alice')
+        self.assertEqual(normal_user.viewname, '_alice')
+        self.assertEqual(normal_user.email, 'alice@example.com')
+        self.assertFalse(normal_user.is_staff)
+        self.assertTrue(normal_user.is_active)
 
     def test_create_valid_user_blank_viewname(self):
-        self.assertEqual(self.blank_viewname_user.viewname, '')
+        blank_viewname_user = UserFactory(viewname='')
+        self.assertEqual(blank_viewname_user.viewname, '')
 
     def test_create_user(self):
         _user = UserModel.objects.create_user(username='user', email='user@example.com', password='password')
@@ -42,7 +44,12 @@ class UserModelTests(TestCase):
     def test_send_mail(self):
         _subject = 'subject name'
         _message = 'sample message'
-        self.normal_user.email_user(_subject, _message)
+        normal_user = UserFactory()
+        normal_user.email_user(_subject, _message)
         _outbox = mail.outbox[0]
         self.assertEqual(_outbox.subject, _subject)
         self.assertEqual(_outbox.body, _message)
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
