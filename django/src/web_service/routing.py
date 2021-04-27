fb63b6f566d58ef42b492595d13f998440562ca1
nas user <pi@nas.localnet.jp>
2021-04-27 20:33:05 +0900

    不要なパスを削除

--- django/src/web_service/routing.py
+++ django/src/web_service/routing.py
@@ -1,8 +1,6 @@
 from django.urls import include, path
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
-from django.urls import path
 from chat import consumers
 
 websocket_urlpatterns = [
